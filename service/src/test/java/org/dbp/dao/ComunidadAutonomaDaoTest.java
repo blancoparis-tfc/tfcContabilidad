78e7132e978e8973aa1f1dee2e294d6e87b1efd9
blancoparis-tfc <blancoparis.tfc@gmail.com>
2016-04-23 23:26:28 +0200

    Test del la comunidad autonoma
    close #70
    
    Test de la comunidad autonoma.

--- service/src/test/java/org/dbp/dao/ComunidadAutonomaDaoTest.java
+++ service/src/test/java/org/dbp/dao/ComunidadAutonomaDaoTest.java
@@ -1,10 +1,14 @@
 package org.dbp.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.dbp.bom.localizacion.ComunidadAutonoma;
 import org.dbp.core.config.TestConfiguracion;
+import org.dbp.dto.localizacion.ComunidadAutonomaFiltro;
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +24,34 @@ public class ComunidadAutonomaDaoTest {
 
 	@Autowired private ComunidadAutonomaDao dao;
 	
+	@Test
+	public void test001Consultar(){
+		final ComunidadAutonoma comunidadAutonoma = dao.obtenerId(1L);
+		assertEquals("Descripcion","Andalucía",comunidadAutonoma.getNombre());
+	}
 	
+	@Test
+	public void test002FiltroFull(){
+		final ComunidadAutonomaFiltro filtro = new ComunidadAutonomaFiltro();
+		List<ComunidadAutonoma> resultado = dao.consultarFiltro(filtro);
+		assertEquals("Se esperan 19 paises",19, resultado.size());
+	}
+
+	@Test
+	public void test003FiltroIdMenor(){
+		final ComunidadAutonomaFiltro filtro = new ComunidadAutonomaFiltro();
+		filtro.setId("<2");
+		List<ComunidadAutonoma> resultado = dao.consultarFiltro(filtro);
+		assertEquals("Se esperan 1 paises",1, resultado.size());
+		assertEquals("Descripcion","Andalucía",resultado.get(0).getNombre());
+	}
 	
+	@Test
+	public void test004FiltroDescripcion(){
+		final ComunidadAutonomaFiltro filtro = new ComunidadAutonomaFiltro();
+		filtro.setNombre("Andalu*");
+		List<ComunidadAutonoma> resultado = dao.consultarFiltro(filtro);
+		assertEquals("Se esperan 1 paises",1, resultado.size());
+		assertEquals("Descripcion","Andalucía",resultado.get(0).getNombre());
+	}
 }
