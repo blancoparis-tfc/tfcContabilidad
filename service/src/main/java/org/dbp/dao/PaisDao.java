b0bdd2a36d2477a9221d8516a2f5fa218a74c92e
blancoparis-tfc <blancoparis.tfc@gmail.com>
2016-04-23 20:43:29 +0200

    Refaztor de los import
    Refaztor de los import

--- service/src/main/java/org/dbp/dao/PaisDao.java
+++ service/src/main/java/org/dbp/dao/PaisDao.java
@@ -2,14 +2,9 @@ package org.dbp.dao;
 
 import java.util.List;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import org.dbp.bom.localizacion.Pais;
 import org.dbp.core.dao.GenericDao;
 import org.dbp.dto.localizacion.PaisFiltro;
-import org.dbp.utils.JqplFilterUtils;
 
 public interface PaisDao extends GenericDao<Pais,String>{
 
