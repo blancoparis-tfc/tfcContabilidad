d0e84e07e53266c0906a5605007170d6115cc43f
blancoparis-tfc <blancoparis.tfc@gmail.com>
2016-04-28 20:40:30 +0200

    Cambios para la mensajeria
    Transitorio del modulo de mensajeria.

--- webapp/src/main/webapp/WEB-INF/cliente/app/core/mensajeria/mensajeria.ts
+++ webapp/src/main/webapp/WEB-INF/cliente/app/core/mensajeria/mensajeria.ts
@@ -1,38 +1,41 @@
-import {Injectable,ElementRef,Component,DynamicComponentLoader,ComponentRef,ViewContainerRef} from 'angular2/core';
+import {Injectable,ElementRef,Component,DynamicComponentLoader,ComponentRef,ViewContainerRef,ApplicationRef} from 'angular2/core';
 import {PromiseWrapper} from 'angular2/src/facade/promise';
 @Injectable()
 export class Mensajeria{
 
   private intervalo:number=10000;
 
-  constructor(private cargador: DynamicComponentLoader){}
+  constructor(private cargador: DynamicComponentLoader, private appRef : ApplicationRef ){}
 
   public success(elemento:ViewContainerRef,mensaje:string){
-      this.cargador.loadAsRoot(DbpMensaje,'#mensajeria',elemento.injector).then(containerRef=>{
+      console.info('success',this.appRef);
+      /*this.cargador.loadAsRoot(DbpMensaje,'#mensajeria',elemento.injector).then(containerRef=>{
         containerRef.instance.mensaje=mensaje;
         containerRef.instance.clase='alert-success';
         containerRef.instance.elemento=containerRef;
-        setTimeout(()=>{containerRef.destroy();},this.intervalo);
-      });
+      //  setTimeout(()=>{containerRef.destroy();},this.intervalo);
+    });*/
   }
 
   public warning(elemento:ViewContainerRef,mensaje:string){
-    this.cargador.loadAsRoot(DbpMensaje,'#mensajeria',elemento.injector).then(containerRef=>{
+    console.info('warning',this.appRef);
+    /*this.cargador.loadAsRoot(DbpMensaje,'#mensajeria',elemento.injector).then(containerRef=>{
       containerRef.instance.mensaje=mensaje;
       containerRef.instance.clase='alert-warning';
       containerRef.instance.elemento=containerRef;
-      setTimeout(()=>{containerRef.destroy();},this.intervalo);
-    });
+    //  setTimeout(()=>{containerRef.destroy();},this.intervalo);
+  });*/
   }
 
 
   public error(elemento:ViewContainerRef,mensaje:string){
-    this.cargador.loadAsRoot(DbpMensaje,'#mensajeria',elemento.injector).then(containerRef=>{
+    console.info('error',this.appRef);
+    /*this.cargador.loadAsRoot(DbpMensaje,'#mensajeria',elemento.injector).then(containerRef=>{
       containerRef.instance.mensaje=mensaje;
       containerRef.instance.clase='alert-danger';
       containerRef.instance.elemento=containerRef;
-      setTimeout(()=>{containerRef.destroy();},this.intervalo);
-    });
+    //  setTimeout(()=>{containerRef.destroy();},this.intervalo);
+  });*/
   }
 }
 
@@ -40,7 +43,7 @@ export class Mensajeria{
   selector:'dbp-mensaje-success',
   template:`<div class="alert {{clase}}"  role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close" (click)="cerrarMensaje()"><span aria-hidden="true">&times;</span></button>
-            {{mensaje}}
+            {{mensaje}} Texto
             </div>`
 })
 export class DbpMensaje{
